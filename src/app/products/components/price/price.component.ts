d18d644425174e80edad1b543eb5dffa11c5b556
William Velandia <wvelandia@gmail.com>
2024-06-19 14:35:41 -0500

    Proyecto lifecyclep - Final - Ciclo de Vida de los Componentes

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Subscribable, Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'products-price',
   templateUrl: './price.component.html',
   styleUrl: './price.component.css'
 })
-export class PriceComponent {
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+
+  @Input()
+  public price: number = 0;
+
+  // Se coloca el signo $ para identificar que esto es un observable
+  public interval$?: Subscription;
+
+  constructor() {}
+
+  // En este ejemplo podemos observar en el log que primero se ejecuta el evento ngOnChanges y después el onInit,
+  // con ellos se pueden usar propiedades en e Input que vienen en el OnInit.
+
+  ngOnInit(): void {
+    console.log('Componente HIJO: ngOnInit');
+
+    // Este interval me va a emitir un numero cada segundo cuando se crea el componente.
+    // Así como esta definida podemos cometer el error de que esto no se destruye y asi se cierre el componente sigue mostrando valores.
+    //interval(1000).subscribe( value => console.log(`Tick: ${value}`) );
+    this.interval$ = interval(1000).subscribe( value => console.log(`Tick: ${value}`) );
+
+    // Cuando se usen los eventos de addEventListener tambien se deben de destruir.
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Componente HIJO: ngOnChanges');
+    console.log({ changes });
+  }
+
+  ngOnDestroy(): void {
+    console.log('Componente HIJO: ngOnDestroy');
+    this.interval$?.unsubscribe(); // Esto me caancela la subscripion del interval.
+  }
 
 }
--- src/app/products/pages/product/product.component.ts
+++ src/app/products/pages/product/product.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'product-product-page',
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+
+  // Vamos a reaccionar con un cambio para que se dispare el Onchange
+  public isProductVisible: boolean = false;
+  public currentPrice: number = 10;
+
+  // Implementamos todos los ciclos de vida de los componentes.
+  // OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy
+  // constructor: se usa para incializaciones, no se deben de usar peticiones Http acá.
+  constructor() {
+    console.log('Constructor');
+  }
+
+  ngOnInit(): void {
+    // Utilizado para hacer la primera petición Http, para hacer los subscribe o escuchar websocket, o implememtar timer.
+    console.log('ngOnInit');
+  }
+
+  // Se usa si necesitamos estar pendientes de los cambios de las propiedades de los @Input
+  ngOnChanges(changes: SimpleChanges): void {
+    // Aca se pueden identifica los cambios que han pasado.
+    console.log('ngOnChanges');
+    console.log({ changes });
+  }
+
+  // ngDoCheck,ngAfterContentInit, ngAfterViewInit, ngAfterViewChecked : Se usan para cuando suceden cambios en @Inputs,
+  // se usan mas que todo por ejemplo en un sistema de graficos, cuando se recalcula por ejemplo la pantalla.
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  //
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+
+  increasePrice() {
+    this.currentPrice++;
+  }
 
 }
